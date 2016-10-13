5d524fade1672e296db8a07534b0da757c990511
Jeroen Engels <jfm.engels@gmail.com>
2016-10-13 18:28:13 +0200

    Migrate linting from standard to XO

--- test.js
+++ test.js
@@ -1,8 +1,8 @@
 const test = require('ava')
-const createThrottle = require('./')
 const sleep = require('then-sleep')
+const createThrottle = require('./')
 
-test('test concurrency of 1', async (t) => {
+test('test concurrency of 1', async t => {
   const throttle = createThrottle(1)
   const pending = []
 
@@ -11,17 +11,19 @@ test('test concurrency of 1', async (t) => {
 
   // measure total executions
   let total = 0
-  let expected = 10
+  const expected = 10
+
+  const fn = async () => {
+    cur++
+    t.is(cur, 1)
+    await sleep(100)
+    cur--
+    t.is(cur, 0)
+    total++
+  }
 
   for (let a = 0; a < expected; a++) {
-    pending.push(throttle(async () => {
-      cur++
-      t.is(cur, 1)
-      await sleep(100)
-      cur--
-      t.is(cur, 0)
-      total++
-    }))
+    pending.push(throttle(fn))
   }
 
   // wait on all tasks
@@ -31,7 +33,7 @@ test('test concurrency of 1', async (t) => {
   t.is(total, expected)
 })
 
-test('test concurrency of 2', async (t) => {
+test('test concurrency of 2', async t => {
   const throttle = createThrottle(2)
   const pending = []
 
@@ -40,21 +42,27 @@ test('test concurrency of 2', async (t) => {
 
   // measure total executions
   let total = 0
-  let expected = 10
+  const expected = 10
   let wasTwo = false
   let wasZero = false
 
+  const fn = async () => {
+    cur++
+    t.true(cur <= 2)
+    if (cur === 2) {
+      wasTwo = true
+    }
+    await sleep(100)
+    cur--
+    t.true(cur <= 2)
+    if (cur === 0) {
+      wasZero = true
+    }
+    total++
+  }
+
   for (let a = 0; a < expected; a++) {
-    pending.push(throttle(async () => {
-      cur++
-      t.true(cur <= 2)
-      if (cur === 2) wasTwo = true
-      await sleep(100)
-      cur--
-      t.true(cur <= 2)
-      if (cur === 0) wasZero = true
-      total++
-    }))
+    pending.push(throttle(fn))
   }
 
   // wait on all tasks
@@ -66,7 +74,7 @@ test('test concurrency of 2', async (t) => {
   t.true(wasTwo)
 })
 
-test('error', (t) => {
+test('error', t => {
   try {
     createThrottle(null)
   } catch (err) {
@@ -74,7 +82,7 @@ test('error', (t) => {
   }
 })
 
-test('return values', async (t) => {
+test('return values', async t => {
   const throttle = createThrottle(1)
   const val = await throttle(async () => {
     await sleep(100)
@@ -83,11 +91,11 @@ test('return values', async (t) => {
   t.is(val, 'haha')
 })
 
-test('errors should not interrupt the queue', async (t) => {
+test('errors should not interrupt the queue', async t => {
   const throttle = createThrottle(1)
   const p1 = throttle(async () => {
     await sleep(100)
-    throw Error('haha')
+    throw new Error('haha')
   })
   const p2 = throttle(async () => {
     await sleep(100)
@@ -95,10 +103,10 @@ test('errors should not interrupt the queue', async (t) => {
   })
 
   // make sure it threw
-  await new Promise((resolve) => {
-    p1.catch((err) => {
+  await new Promise(resolve => {
+    p1.catch(err => {
       t.true(err instanceof Error)
-      p2.then((data) => {
+      p2.then(data => {
         t.is(data, 'woot')
         resolve()
       })
