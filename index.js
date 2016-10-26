c7e66cd0890e1537c9410d8822635b30a14a40a3
Nathan Rajlich <nathan@tootallnate.net>
2016-10-26 10:47:05 -0700

    refactor / simplify logic

--- index.js
+++ index.js
@@ -10,36 +10,29 @@ module.exports = function (max) {
   return function (fn) {
     return new Promise((resolve, reject) => {
       function handleFn() {
-        cur++
-        fn()
-          .then(val => {
-            resolve(val)
-            cur--
-            if (queue.length > 0) {
-              queue.shift()()
-            }
-          })
-          .catch(err => {
-            reject(err)
-            cur--
-            if (queue.length > 0) {
-              queue.shift()()
-            }
-          })
+        if (cur < max) {
+          cur++
+          fn()
+            .then(val => {
+              resolve(val)
+              cur--
+              if (queue.length > 0) {
+                queue.shift()()
+              }
+            })
+            .catch(err => {
+              reject(err)
+              cur--
+              if (queue.length > 0) {
+                queue.shift()()
+              }
+            })
+        } else {
+          queue.push(handleFn)
+        }
       }
 
-      if (cur < max) {
-        handleFn()
-      } else if (cur < max) {
-        // avoid a race condition where the
-        // concurrency went down prior to this
-        // promise being executed in a microtask
-        handleFn()
-      } else {
-        queue.push(() => {
-          handleFn()
-        })
-      }
+      handleFn()
     })
   }
 }
